10fa68826e56571660b2b3a0ac9cc364a61112cf
Aaron.rklsImmersion <awinebrenner@gmail.com>
2018-08-02 23:53:43 -0500

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# UjuComics
+Here's where you'll find our game stuff. Just, you know, not all of it.
+
+Go to the Google Drive for the big stuff, and the stuff which doesn't directly effect the UE4 project. Everything else, info wise, is on HacknPlan
---
+++ Source/UjuComics/UjuComicsGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UjuComicsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UJUCOMICS_API AUjuComicsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
